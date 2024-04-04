4aa6833351420423068f1fa2afe4f23b9bf037e0
AhmadCheaito <ahmad1998cheaito@gmail.com>
2024-04-04 11:09:21 -0400

    Structure du projet #2

--- database.py
+++ database.py
@@ -1,20 +1,48 @@
+import os
+
+import cursor
 import pymysql
+from dotenv import load_dotenv
 
-connection = pymysql.connect(
-    host="localhost",
-    user="root",
-    password="cheaito",
-    db="labo6",
-    autocommit=True
-)
 
-cursor = connection.cursor()
+# from sql_utils import run_sql_file
+
+
+class Database:
+    def __init__(self):
+        """
+            Chargez les variables d'environnement de votre fichier .env, puis complétez les lignes 15 à 19 afin de récupérer les valeurs de ces variables
+        """
+        load_dotenv()
+        self.host = os.environ.get("HOST")
+        self.port = os.environ.get("PORT")
+        self.database = os.environ.get("DATABASE")
+        self.user = os.environ.get("USER")
+        self.password = os.environ.get("PASSWORD")
+
+        self._open_sql_connection()
+
+        self.migration_counter = 0
+
+    def _open_sql_connection(self):
+        self.connection = pymysql.connect(
+            host=self.host,
+            port=int(self.port),
+            user=self.user,
+            password=self.password,
+            db=self.database,
+            autocommit=True
+        )
+
+        self.cursor = self.connection.cursor()
+
 
 
 def insert_todo(text):
     request = f"""INSERT INTO todo (text) VALUE ("{text}")"""
 
     cursor.execute(request)
+
 
 if __name__ == '__main__':
     create_table = "CREATE TABLE todo(id INTEGER AUTO_INCREMENT, text VARCHAR(400), PRIMARY KEY(id));"
--- server.py
+++ server.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+
+from database import Database
+
+database = Database()
 
 app = Flask(__name__)
 
