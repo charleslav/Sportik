04777d1d5c6140c3a909de105569f73b8fbbe59a
AhmadCheaito <ahmad1998cheaito@gmail.com>
2024-04-05 15:21:34 -0400

    Pour la generation des donnes de la database

--- database.py
+++ database.py
@@ -57,6 +57,48 @@ def generate_customer_data(cursor, fake):
             VALUES("{name}", '{username}', '{password}', '{age}', '{email}', '{customer_adress}');"""
         cursor.execute(request)
 
+def generate_product_data(cursor, fake):
+    for i in range(10):
+        product_name = "Jordan"
+        product_rating = random.randint(0,5)
+        product_image = "Sportik/images/Jordan.png"
+        description = "Air Jordan is a renowned line of basketball shoes and athletic clothing produced by Nike. It was created for former NBA player Michael Jordan and released to the public on April 1, 1985.The shoes were designed by Peter Moore, Tinker Hatfield, and Bruce Kilgore, and are known for their innovative design, performance, and cultural impact."
+        request = f"""INSERT INTO Product (product_name, product_rating, product_image, description)
+        VALUES('{product_name}', {product_rating}, '{product_image}', '{description}');"""
+        cursor.execute(request)
+    for i in range(10):
+        product_name = "Adidas"
+        product_rating = random.randint(0,5)
+        product_image = "Sportik/images/Adidas.png"
+        description = "Adidas AG is a German athletic apparel and footwear corporation headquartered in Herzogenaurach, Bavaria, Germany. It is the largest sportswear manufacturer in Europe, and the second largest in the world, after Nike."
+        request = f"""INSERT INTO Product (product_name, product_rating, product_image, description)
+        VALUES('{product_name}', {product_rating}, '{product_image}', '{description}');"""
+        cursor.execute(request)
+    for i in range(10):
+        product_name = "Under Armour"
+        product_rating = random.randint(0,5)
+        product_image = "Sportik/images/Under_Armour.png"
+        description = "Under Armour, Inc. is an American sportswear company that manufactures footwear and apparel headquartered in Baltimore, Maryland, United States. Under Armour, Inc."
+        request = f"""INSERT INTO Product (product_name, product_rating, product_image, description)
+        VALUES('{product_name}', {product_rating}, '{product_image}', '{description}');"""
+        cursor.execute(request)
+    for i in range(10):
+        product_name = "Puma"
+        product_rating = random.randint(0, 5)
+        product_image = "Sportik/images/Puma.png"
+        description = "Puma SE is a German multinational corporation that designs and manufactures athletic and casual footwear, apparel, and accessories, headquartered in Herzogenaurach, Bavaria, Germany. Puma is the third largest sportswear manufacturer in the world."
+        request = f"""INSERT INTO Product (product_name, product_rating, product_image, description)
+        VALUES('{product_name}', {product_rating}, '{product_image}', '{description}');"""
+        cursor.execute(request)
+    for i in range(10):
+        product_name = "New Balance"
+        product_rating = random.randint(0, 5)
+        product_image = "Sportik/images/New Balance.png"
+        description = "(NB), best known as simply New Balance, is one of the worlds major sports footwear and apparel manufacturers. Based in Boston, Massachusetts, the multinational corporation was founded in 1906 as the New Balance Arch Support Company."
+        request = f"""INSERT INTO Product (product_name, product_rating, product_image, description)
+        VALUES('{product_name}', {product_rating}, '{product_image}', '{description}');"""
+        cursor.execute(request)
+
 
 
 if __name__ == '__main__':
@@ -65,5 +107,6 @@ if __name__ == '__main__':
     fake = Faker()
     cursor = db.cursor  # Access the cursor from the Database instance
     generate_customer_data(cursor, fake)
+    generate_product_data(cursor, fake)
     cursor.execute(create_table)
 
--- sql/DB_up.sql
+++ sql/DB_up.sql
@@ -24,20 +24,20 @@ CREATE TABLE IF NOT EXISTS Category (catid integer AUTO_INCREMENT NOT NULL,
                        PRIMARY KEY (catid), #CATEGORIE DUNE CATÉGORIE
                        FOREIGN KEY (parent_category_id) REFERENCES Category(catid));
 ALTER TABLE Category AUTO_INCREMENT=1000000;
-INSERT INTO Category(catid,category_name,parent_category_id, is_active) VALUES ();
+#INSERT INTO Category(catid,category_name,parent_category_id, is_active) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Customer (cid integer AUTO_INCREMENT NOT NULL,
                        name varchar(35) NOT NULL,
                        username varchar(50) UNIQUE NOT NULL,
                        password varchar(50) NOT NULL,
-                       age tinyint NOT NULL check(age > 16),
+                       age tinyint NOT NULL check(age >= 16),
                        email varchar(40) UNIQUE NOT NULL,
                        customer_adress varchar(255) UNIQUE NOT NULL,
                        creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
                        card_total_price integer DEFAULT 0,
                        PRIMARY KEY(cid));
 ALTER TABLE Customer AUTO_INCREMENT=2000000;
-INSERT INTO Customer(cid, name, username, password, age, email, customer_adress, creation_date, card_total_price) VALUES ();
+#INSERT INTO Customer(cid, name, username, password, age, email, customer_adress, creation_date, card_total_price) VALUES ();
 
 #Changer order et regarder le datatype de is_featured pour le bool
 CREATE TABLE IF NOT EXISTS Provider (provider_id integer AUTO_INCREMENT NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS Provider (provider_id integer AUTO_INCREMENT NOT NULL
                        featured_image varchar(250) NOT NULL,
                        PRIMARY KEY (provider_id));
 ALTER TABLE  Provider AUTO_INCREMENT=3000000;
-INSERT INTO Provider(provider_id, provider_name, is_featured, provider_order_number, featured_image) VALUES ();
+#INSERT INTO Provider(provider_id, provider_name, is_featured, provider_order_number, featured_image) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Product_Packaging (ppid integer AUTO_INCREMENT NOT NULL,
                                dimension ENUM('kg', 'lb') NOT NULL DEFAULT 'kg',
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS Product_Packaging (ppid integer AUTO_INCREMENT NOT NU
                                packaging_material varchar(30) NOT NULL,
                                PRIMARY KEY (ppid));
 ALTER TABLE Product_Packaging AUTO_INCREMENT=3000000;
-INSERT INTO Product_Packaging(ppid, dimension, weight, packaging_material) VALUES ();
+#INSERT INTO Product_Packaging(ppid, dimension, weight, packaging_material) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Discount (did integer AUTO_INCREMENT NOT NULL,
                       discount_rate decimal DEFAULT NULL,
@@ -65,17 +65,7 @@ CREATE TABLE IF NOT EXISTS Discount (did integer AUTO_INCREMENT NOT NULL,
                       is_active BOOLEAN DEFAULT FALSE,                               /* va etre activer comme true lorsque start_date va etre hit, false lorsque end_date va hit */
                       PRIMARY KEY(did));
 ALTER TABLE Discount AUTO_INCREMENT=4000000;
-INSERT INTO Discount(did, discount_rate, discount_amount, start_date, end_date) VALUES ();
-
-CREATE TABLE IF NOT EXISTS Product_Model_Image (product_model_image_id INTEGER AUTO_INCREMENT NOT NULL,
-                                product_model_id integer NOT NULL,
-                                image_type ENUM('thumbnail', 'main', 'gallery', 'zoomed') NOT NULL DEFAULT 'main', /*selectionner on veut presenter les images a quel endroit
-                   /*              thumbnail varchar(250) NOT NULL,                      a enlever probablement */
-                                 image varchar(250) NOT NULL,
-                                 PRIMARY KEY(product_model_image_id),
-                                 FOREIGN KEY(product_model_id) REFERENCES Product_Model(pmid));
-ALTER TABLE Product_Model_Image AUTO_INCREMENT=5000000;
-INSERT INTO Product_Model_Image(product_model_id, image_type, image) VALUES ();
+#INSERT INTO Discount(did, discount_rate, discount_amount, start_date, end_date) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Orders (order_id integer AUTO_INCREMENT NOT NULL,
                      payment_method varchar(20) NOT NULL,
@@ -84,19 +74,19 @@ CREATE TABLE IF NOT EXISTS Orders (order_id integer AUTO_INCREMENT NOT NULL,
                      order_status ENUM ('Succes', 'In Progress', 'Canceled') NOT NULL,
                      PRIMARY KEY (order_id));
 ALTER TABLE Orders AUTO_INCREMENT=6000000;
-INSERT INTO Orders(order_id, payment_method, payment_status, order_date, order_status) VALUES ();
+#INSERT INTO Orders(order_id, payment_method, payment_status, order_date, order_status) VALUES ();
 #Creation des tables #2
 CREATE TABLE IF NOT EXISTS Product(pid integer AUTO_INCREMENT NOT NULL,
                      product_name varchar(100) NOT NULL,
                      product_rating integer NOT NULL,
                      product_image varchar(250) NOT NULL,
                      description varchar(2000) NOT NULL,
-                     provider_id INTEGER NOT NULL,
+                     provider_id INTEGER,
                      PRIMARY KEY (pid),
                      FOREIGN KEY (provider_id) REFERENCES Provider(provider_id),
                      constraint CT_Rating_Range CHECK (product_rating BETWEEN 0 AND 5));
 ALTER TABLE Product AUTO_INCREMENT=7000000;
-INSERT INTO Product(pid, product_name, product_rating, product_image, description, provider_id) VALUES ();
+#INSERT INTO Product(pid, product_name, product_rating, product_image, description, provider_id) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Product_Model (pmid integer AUTO_INCREMENT NOT NULL,
                            product_name varchar(200) UNIQUE NOT NULL,
@@ -112,7 +102,7 @@ CREATE TABLE IF NOT EXISTS Product_Model (pmid integer AUTO_INCREMENT NOT NULL,
                            FOREIGN KEY (discount_id) REFERENCES Discount(did),
                            FOREIGN KEY (packaging_id) REFERENCES Product_Packaging(ppid));
 ALTER TABLE Product_Model AUTO_INCREMENT=8000000;
-INSERT INTO Product_Model(pmid, product_name, price, quantity, product_id, discount_id, packaging_id, upc, sku) VALUES ();
+#INSERT INTO Product_Model(pmid, product_name, price, quantity, product_id, discount_id, packaging_id, upc, sku) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Customer_review (crid integer AUTO_INCREMENT NOT NULL,
                               customer_id integer,
@@ -123,7 +113,7 @@ CREATE TABLE IF NOT EXISTS Customer_review (crid integer AUTO_INCREMENT NOT NULL
                               FOREIGN KEY (customer_id) REFERENCES Customer(cid),
                               FOREIGN KEY (productModel_id) REFERENCES Product_Model(pmid));#STARS ?
 ALTER TABLE Customer_review AUTO_INCREMENT=9000000;
-INSERT INTO Customer_review(crid, customer_id, productModel_id, created_at, item_rating_review) VALUES ();
+#INSERT INTO Customer_review(crid, customer_id, productModel_id, created_at, item_rating_review) VALUES ();
 
 CREATE TABLE IF NOT EXISTS Cart  (pmid integer NOT NULL,
                     cid integer NOT NULL,
@@ -133,7 +123,17 @@ CREATE TABLE IF NOT EXISTS Cart  (pmid integer NOT NULL,
                     FOREIGN KEY (pmid) REFERENCES Product_Model(pmid),
                     FOREIGN KEY (cid) REFERENCES Customer(cid));
 ALTER TABLE Cart AUTO_INCREMENT=10000000;
-INSERT INTO Cart(pmid, cid, total_amount, quantity, item_discount_total) VALUES ();
+#INSERT INTO Cart(pmid, cid, total_amount, quantity, item_discount_total) VALUES ();
+
+CREATE TABLE IF NOT EXISTS Product_Model_Image (product_model_image_id INTEGER AUTO_INCREMENT NOT NULL,
+                                product_model_id integer NOT NULL,
+                                image_type ENUM('thumbnail', 'main', 'gallery', 'zoomed') NOT NULL DEFAULT 'main', /*selectionner on veut presenter les images a quel endroit
+                   /*              thumbnail varchar(250) NOT NULL,                      a enlever probablement */
+                                 image varchar(250) NOT NULL,
+                                 PRIMARY KEY(product_model_image_id),
+                                 FOREIGN KEY(product_model_id) REFERENCES Product_Model(pmid));
+ALTER TABLE Product_Model_Image AUTO_INCREMENT=5000000;
+#INSERT INTO Product_Model_Image(product_model_id, image_type, image) VALUES ();
 
 /*
  Cette table permet de regrouper le Product et sa Categorie dans une table pour savoir quel est la categorie d'un produit
@@ -143,7 +143,7 @@ CREATE TABLE IF NOT EXISTS Product_isClassifiedAs_Category (pid integer NOT NULL
                                               FOREIGN KEY (pid) REFERENCES Product(pid),
                                               FOREIGN KEY (catid) REFERENCES Category(catid));
 ALTER TABLE Product_isClassifiedAs_Category AUTO_INCREMENT=11000000;
-INSERT INTO Product_isClassifiedAs_Category(pid, catid) VALUES ();
+#INSERT INTO Product_isClassifiedAs_Category(pid, catid) VALUES ();
 
 /*
  Cette table permet de regrouper le Product Model et son type de Produit dans une table pour savoir quel variation de produit est associe
@@ -154,4 +154,4 @@ CREATE TABLE IF NOT EXISTS ProductModel_ISA_Product (product_model_id integer NO
                                        FOREIGN KEY (product_model_id) REFERENCES Product_Model(pmid),
                                        FOREIGN KEY (product_id) REFERENCES Product(pid));
 ALTER TABLE ProductModel_ISA_Product AUTO_INCREMENT=12000000;
-INSERT INTO ProductModel_ISA_Product(product_model_id, product_id) VALUES ();
+#INSERT INTO ProductModel_ISA_Product(product_model_id, product_id) VALUES ();
