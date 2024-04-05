d1eb2fadc217c009983b9409b494a14c9a313392
AhmadCheaito <ahmad1998cheaito@gmail.com>
2024-04-05 13:07:31 -0400

    Pour la generation des donnes de la database

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 import os
+import random
 
 import cursor
+from faker import Faker
+
 import pymysql
 from dotenv import load_dotenv
 
-
-# from sql_utils import run_sql_file
+#from sql_utils import run_sql_file
 
 
 class Database:
@@ -37,14 +39,31 @@ class Database:
         self.cursor = self.connection.cursor()
 
 
-
 def insert_todo(text):
     request = f"""INSERT INTO todo (text) VALUE ("{text}")"""
 
     cursor.execute(request)
 
 
-if __name__ == '__main__':
-    create_table = "CREATE TABLE todo(id INTEGER AUTO_INCREMENT, text VARCHAR(400), PRIMARY KEY(id));"
+def generate_customer_data(cursor, fake):
+    for i in range(100):
+        name = fake.unique.name()
+        username = fake.unique.user_name()
+        password = fake.password()
+        email = fake.unique.email()
+        age = random.randint(16, 127)
+        customer_adress = fake.address()
+        request = f"""INSERT INTO Customer (name, username, password, age, email, customer_adress)
+            VALUES("{name}", '{username}', '{password}', '{age}', '{email}', '{customer_adress}');"""
+        cursor.execute(request)
 
+
+
+if __name__ == '__main__':
+    db = Database()  # Create an instance of the Database class
+    create_table = "CREATE TABLE todo(id INTEGER AUTO_INCREMENT, text VARCHAR(400), PRIMARY KEY(id));"
+    fake = Faker()
+    cursor = db.cursor  # Access the cursor from the Database instance
+    generate_customer_data(cursor, fake)
     cursor.execute(create_table)
+
