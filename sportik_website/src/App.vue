85bcae041926be8da02e2e45666508a7c85ab9a1
charleslav <charles01_lv-2000@hotmail.com>
2024-04-10 07:43:53 -0400

    file for good running

---
+++ sportik_website/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <navbar_website/>
+  <router-view></router-view>
+</template>
+
+<script>
+import navbar_website from './components/navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    navbar_website
+  }
+}
+
+
+
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+</style>
